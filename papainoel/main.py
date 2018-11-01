1f716355dc96a31140e2e981b9428a34366356a3
brunoqs <brun_o13@hotmail.com>
2018-11-01 11:51:17 -0300

    condição de parada adicionada

--- papainoel/main.py
+++ papainoel/main.py
@@ -1,34 +1,45 @@
 import random
 import time
+import sys
 
 from threading import Thread, Event, Semaphore
 
 n_renas = 9
 n_elfos = 10
 
+anos = n_renas
+
 # eventos que deixarao renas, elfos e noel esperando
 despertador_noel = Event()
 despertador_rena = Event()
 despertador_elfo = Event()
 
-# semaforo para incremento e decremento de renas e elfos
+# semaforo para incremento de renas e elfos
 semaforo = Semaphore()
 
 count_rena = 0
 count_elfo = 0
+count_loop = 0
 
 # fila de renas e elfos que foram executados
 fila_rena = []
 fila_elfo = []
 
-
 def noel():
+    ''' Funcao sera executada quando 9 threads renas ou 3 threads elfo estiverem prontas,
+    ela para de acordo com os anos setados na entrada no programa '''
     while True:
         if len(fila_rena) == 9:
             print("Papai noel lacou as renas " + str(fila_rena))
             despertador_rena.set()
             fila_rena.clear()
             count_rena = 0
+
+            if count_loop == anos:
+                print("Papai noel dormindo.")
+                despertador_elfo.set()
+                break
+
         elif len(fila_elfo) == 3:
             print("Papai noel reuniu com os elfos " + str(fila_elfo))
             despertador_elfo.set()
@@ -40,13 +51,16 @@ def noel():
         despertador_noel.wait()
         print("Papai noel acordado.")
 
-
 def rena(id):
-    global count_rena
+    ''' Funcao acorda a thread papai noel quando 9 threads renas estiverem prontas, ela so ira parar quando 
+    as 9 threads renas forem executadas de acordo com os anos '''
+    global count_rena, count_loop
     while True:
         time.sleep(random.random() * 3)
+        if count_loop == anos:
+            break
+        
         semaforo.acquire()
-
         print("Rena: " + str(id))
         if count_rena == 8:
             print("9 renas")
@@ -57,18 +71,24 @@ def rena(id):
             count_rena += 1
             fila_rena.append(id)
 
+        count_loop += 1
         semaforo.release()
+
         despertador_rena.clear()
         despertador_rena.wait()
         print('Rena ' + str(id) + ' esta de ferias')
 
-
 def elfo(id):
+    ''' Funcao acorda a thread papai noel quando 3 threads renas estiverem prontas (da prioridade as renas), ela so ira parar
+    quando as threds renas ja tiverem parado '''
     global count_elfo
     while True:
         time.sleep(random.random() * 3)
-        semaforo.acquire()
+        if count_loop == anos:
+            print('Elfo ' + str(id) + ' foi fazer brinquedo')
+            break
 
+        semaforo.acquire()
         print("Elfo: " + str(id))
         if count_elfo == 2:
             if len(fila_rena) < 9:
@@ -81,13 +101,19 @@ def elfo(id):
             fila_elfo.append(id)
 
         semaforo.release()
+
         despertador_elfo.clear()
         despertador_elfo.wait()
         print('Elfo ' + str(id) + ' foi fazer brinquedo')
 
-
 if __name__ == "__main__":
-    # criando e comecando a thread papei noel
+    if len(sys.argv) <= 1:
+        print("Informe o numero de anos")
+        sys.exit()
+
+    anos = anos * int(sys.argv[1])
+
+    # criando e comecando a thread papai noel
     noel = Thread(target=noel)
     noel.start()
 
